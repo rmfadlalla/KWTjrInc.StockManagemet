0d476fe46c8ee88a1776b374948f4369221c2772
rmfadlalla <98051639+rmfadlalla@users.noreply.github.com>
2022-11-10 16:38:59 -0500

    Update SaleTester.java

--- src/test/SaleTester.java
+++ src/test/SaleTester.java
@@ -12,7 +12,7 @@ public class SaleTester {
         s1.setSaleAmount(46.99);
         s1.setDate("10.31.22");
         System.out.println(s1.getSaleID() + " " + s1.getCustomerID() + " " + s1.getItemID() + " " + s1.getQuantity() + " " + s1.getSaleAmount() + " " + s1.getDate());
-        Sale s2 = new Sale(003, 76, 654321, 6, 22.22, "9");
+        Sale s2 = new Sale(003, 76, 654321, 6, 22.22, "09.11.21");
         System.out.println(s2.getSaleID() + " " + s2.getCustomerID() + " " + s2.getItemID() + " " + s2.getQuantity() + " " + s2.getSaleAmount() + " " + s2.getDate());
     }
 }
